c270c83cf4005eba248d5a8abc30d8fc6c1dc330
owleek <owleek@MacBook-Pro-owleek.local>
2026-04-23 23:08:46 +0300

    feat(common/http): http versions

--- src/app/common/page.tsx
+++ src/app/common/page.tsx
@@ -4,6 +4,7 @@ import { TileGrid, TileLink } from '@/shared/ui/Tile';
 export default function Javascript() {
   return (
     <TileGrid>
+      <TileLink href='/common/http'>HTTP</TileLink>
       <TileLink href='/common/url'>URL</TileLink>
       <TileLink href='/common/browser'>Browser</TileLink>
       <TileLink href='/common/eventLoop'>EventLoop</TileLink>
